9f47590ea80a71f90a7c88da7a062c71714b3c39
Hubert Suchorowski <hubert.suchorowski1@gmail.com>
2024-05-21 18:26:52 +0100

    Completed front-end.

--- futbol.php
+++ futbol.php
@@ -3,9 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Rozgrywki futbolowe</title>
+    <link rel="stylesheet" href="styl.css">
 </head>
 <body>
-    
+    <header>
+        <div class="baner">
+            <h2>Światowe rozgrywki piłkarskie<h2>
+            <img src="obraz1.jpg" alt="boisko">
+        </div>
+    <header>
+
+    <content>
+        <div class="mecze">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu dui libero. Maecenas in commodo ipsum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+        </div>
+    </content>
+
+    <main>
+        <div class="główny">
+            <h2>Reprezentacja Polski”</h2>
+        </div>
+    </main>
+
+    <section>
+        <div class="lewy">
+            <p> Podaj pozycję zawodników (1-bramkarze, 2-obrońcy, 3-pomocnicy, 4-napastnicy) </p>
+        </div>
+        <div class="prawy">
+            <img src="zad1.png" alt="piłkarz">
+            <p>Autor:00000000000</p>
+        </div>
+    </section>
+
 </body>
 </html>
